afc406b518692831f8db2e0123bde3359aa148eb
MEZIANI Youssef <mezianiyoussef564@gmail.com>
2024-01-09 21:01:18 +0100

    the create function has been added with all possible unit tests + function to save to Gson

--- src/test/java/com/examen_ci/gestion_produit/ProduitTest.java
+++ src/test/java/com/examen_ci/gestion_produit/ProduitTest.java
@@ -1,6 +1,88 @@
 package com.examen_ci.gestion_produit;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class ProduitTest 
 {
-    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /**
+     * Unit test for create product.
+     */
+    @Test
+    public void createWithInvalidPrice()
+    {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("invalid");
+
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", -200, 10));
+    }
+    @Test
+    public void createWithInvalidQuantity()
+    {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("invalid");
+
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", 200, -10));
+    }
+    @Test
+    public void createWithInvalidPriceAndQuantity()
+    {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("invalid");
+
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", -200, -10));
+    }
+
+    @Test
+    public void createWithExistingId()
+    {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("exist");
+
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", 200, 10));
+        ProduitService.create(new Produit(Long.parseLong("1"), "keyborad", 400, 8));
+    }
+    @Test
+    public void createWithExistingName()
+    {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("exist");
+
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", 200, 10));
+        ProduitService.create(new Produit(Long.parseLong("2"), "mouse", 400, 8));
+    }
+    @Test
+    public void createWithExistingIdAndName()
+    {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("exist");
+
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", 200, 10));
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", 400, 8));
+    }
+
+    @Test
+    public void createValidAndNonExisting()
+    {
+        ProduitService.create(new Produit(Long.parseLong("1"), "mouse", 200, 10));
+        ProduitService.create(new Produit(Long.parseLong("2"), "keyborad", 400, 8));
+        assertTrue(true);
+    }
+
+    @After
+    public void After()
+    {
+        File file = new File("products.json");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
